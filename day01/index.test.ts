1e7c0caabc964a936a5d70a3f1243d78c9d89983
thibben <oskar.thibblin@gmail.com>
2023-12-02 09:49:15 +0100

    test fix

--- day01/index.test.ts
+++ day01/index.test.ts
@@ -5,13 +5,7 @@ describe('Bun Tests Examples', () => {
   test('Part 1', () => {
     expect(day01.part1()).toBe(54877)
   })
-  test.skip('Part 2', () => {
-    expect(day01.part2()).toBe(56154)
-  })
-  test('test replace', () => {
-    expect(replaceWithNumber('onetwothreefourfivesixseveneightnine')).toBe("123456789")
-  })
-  test('test replace', () => {
-    expect(replaceWithNumber('nvvxfxbgldrb2seven7twokxzbfkvptflnhlqjrthreeoneights')).toBe("nvvxfxbgldrb2772kxzbfkvptflnhlqjr318s")
+  test('Part 2', () => {
+    expect(day01.part2()).toBe(54100)
   })
 })
